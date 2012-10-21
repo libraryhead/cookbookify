101c4d67a69677cd26cc650d841591579380cf85
Peter Fry <peter@ourstage.com>
2012-10-21 16:54:04 -0400

    Fix recipe creation to assign user_id to the current_user.id.

--- app/admin/recipes.rb
+++ app/admin/recipes.rb
@@ -1,3 +1,19 @@
 ActiveAdmin.register Recipe do
+  before_filter do |controller|
+    if controller.action_name.to_sym == :create
+      params[:recipe][:user_id] = current_user.id
+    end
+  end
   
+  filter :name
+  filter :source_uri
+
+  form do |f|
+    f.inputs "Recipe Details" do
+      f.input :name
+      f.input :source_uri
+      f.input :note
+    end
+    f.buttons
+  end
 end
