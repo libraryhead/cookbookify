d30e6c01a05e0ad1c597323440835a1d44397db4
Rebecca <libraryhead@gmail.com>
2012-10-21 13:34:49 -0400

    Initial Submission

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cookbookify::Application.config.secret_token = 'db9b660278dbd98d5ad4715d66de5f050c63cd5961f36f8c87166425ca68965f676f3f5b5aafb029456601b620351906cef006ac000b4c355913630a2f1e707e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Cookbookify::Application.config.session_store :cookie_store, key: '_cookbookify_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Cookbookify::Application.config.session_store :active_record_store
