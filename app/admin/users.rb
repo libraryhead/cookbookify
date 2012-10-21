31492dc2617f7c9a2f5001fa450c621e17574671
Rebecca <libraryhead@gmail.com>
2012-10-21 16:05:37 -0400

    add active admin

---
+++ app/admin/users.rb
@@ -0,0 +1,21 @@
+ActiveAdmin.register User do     
+  index do                            
+    column :email                     
+    column :current_sign_in_at        
+    column :last_sign_in_at           
+    column :sign_in_count             
+    default_actions                   
+  end                                 
+
+  filter :email                       
+
+  form do |f|                         
+    f.inputs "User Details" do       
+      f.input :email                  
+      f.input :password               
+      f.input :password_confirmation 
+      f.input :is_admin 
+    end                               
+    f.buttons                         
+  end                                 
+end                                   
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  def is_admin_authorized?
+    current_user && current_user.is_admin?
+  end
+  helper_method :is_admin_authorized?
+
+  def authorize_admin!
+    raise 'nope' unless is_admin_authorized?
+  end
 end
