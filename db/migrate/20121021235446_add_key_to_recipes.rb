ec46282ab627fd59e3aa78b3c42a87c55dcb0367
Peter Fry <peter@ourstage.com>
2012-10-21 20:09:32 -0400

    Make Sqlite3 happy.

--- db/migrate/20121021235446_add_key_to_recipes.rb
+++ db/migrate/20121021235446_add_key_to_recipes.rb
@@ -1,6 +1,6 @@
 class AddKeyToRecipes < ActiveRecord::Migration
   def change
-    add_column :recipes, :key, :string, :limit => 12, :null => false
+    add_column :recipes, :key, :string, :limit => 12 #,:null => false
     
     add_index :recipes, :key, :unique => true
   end
